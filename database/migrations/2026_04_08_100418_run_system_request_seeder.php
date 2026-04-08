bd9471bcd2185e1520b23893b7ef7b23bc32d396
elbragi <elbragi8@gmail.com>
2026-04-08 10:05:21 +0300

    Feat: Automate data import and add compiled assets for Task Tracker

---
+++ database/migrations/2026_04_08_100418_run_system_request_seeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Artisan;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Run the seeder to import the Task Tracker data on production
+        Artisan::call('db:seed', [
+            '--class' => 'SystemRequestSeeder',
+            '--force' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
